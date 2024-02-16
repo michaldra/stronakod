7092665047ffa8a13e73610e335d2e2905380942
michaldra <mich.dra10@gmail.com>
2024-02-16 17:18:26 +0100

    hanana
    aaa

---
+++ script.js
@@ -0,0 +1,10 @@
+let number = 15
+let number1 = 30
+let number2 = 16
+console.log("My variable is " + number)
+
+let title = document.querySelector('h1')
+let titleClass = document.querySelector(".titlejs")
+
+title.textContent = "cześć z javascript!"
+titleClass.style.color = "blue"
---
+++ skrypt.js
@@ -0,0 +1,2 @@
+let kolor = document.querySelector(".home__title")
+kolor.style.color = "#ac67e6"
